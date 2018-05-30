3645c92c858888d3e014adad78287b19b4445d6a
Taylor Williams <taylor@thewilliams.net>
2018-05-30 18:15:38 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,8 @@
 class Artist
   attr_accessor :name, :songs
 
+  @@total_songs = 0
+
   def initialize (name)
     @name = name
     @songs = []
@@ -9,16 +11,18 @@ class Artist
   def add_song(song)
     song.artist = self
     songs << song
+    @@total_songs += 1
   end
 
   def add_song_by_name(song)
     new_song = Song.new(song)
     new_song.artist = self
     songs << new_song
+    @@total_songs += 1
     new_song
   end
 
   def self.song_count
-    return self.songs[0].all_songs
+    @@total_songs
   end
 end
