cef52611b0fa88202c9a1830f7aae036f46f35f8
Taylor Williams <taylor@thewilliams.net>
2018-05-30 17:49:42 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,12 +1,12 @@
 class Artist
-  attr_accessor :name :artist :songs
+  attr_accessor :name, :songs
 
   def initialize (name)
     @name = name
   end
 
-  def add_song(song_name, artist)
-
+  def add_song(song)
+    song.artist = self
   end
 
   def add_song_by_name(song_name)
